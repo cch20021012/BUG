cf926a9aa7bd504d4c3f05b2e9ea92249ba70c35
ChenCh <cch20021012@163.com>
2020-12-31 11:35:07 +0800

    顶级工程师科研项目 月薪三万

--- app/src/main/java/com/example/gug/MainActivity.java
+++ app/src/main/java/com/example/gug/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.gug;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.gug.cch.ui.class1.ClassFragment;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vp_main;
+    private TabLayout tl_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        vp_main = (ViewPager) findViewById(R.id.vp_main);
+        tl_main = (TabLayout) findViewById(R.id.tl_main);
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new ClassFragment());
+        fragments.add(new ClassFragment());
+        fragments.add(new ClassFragment());
+        fragments.add(new ClassFragment());
+        vp_main.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+        tl_main.setupWithViewPager(vp_main);
+        tl_main.getTabAt(0).setText("首页").setIcon(R.drawable.home_select_item).isSelected();
+        tl_main.getTabAt(1).setText("课程").setIcon(R.drawable.class_select_item);
+        tl_main.getTabAt(2).setText("活动").setIcon(R.drawable.activity_select_item);
+        tl_main.getTabAt(3).setText("我的").setIcon(R.drawable.me_select_item);
     }
 }
---
+++ app/src/main/java/com/example/gug/cch/ui/class1/ClassFragment.java
@@ -0,0 +1,26 @@
+package com.example.gug.cch.ui.class1;
+
+import com.example.gug.base.BaseFragment;
+import com.example.gug.interfaces.IBasePresenter;
+
+public class ClassFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
