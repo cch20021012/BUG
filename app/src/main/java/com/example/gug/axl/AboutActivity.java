d39d6d55e8cb3ad5a445cab4215759912d4c48a0
Ein-xinlong <wasdijkljnm.com@qq.com>
2020-12-31 10:50:18 +0800

    axl提交3个页面

---
+++ app/src/main/java/com/example/gug/axl/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.example.gug.axl;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gug.R;
+
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.axl_activity_about);
+    }
+}
