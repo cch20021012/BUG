39b8ddc6ca0bced7d6b0f8da740aac01d54aa2c3
ChenCh <cch20021012@163.com>
2020-12-31 12:06:29 +0800

    顶级工程师科研项目 月薪三万

--- app/src/main/java/com/example/gug/cch/ui/class1/ClassFragment.java
+++ app/src/main/java/com/example/gug/cch/ui/class1/ClassFragment.java
@@ -1,12 +1,23 @@
 package com.example.gug.cch.ui.class1;
 
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.gug.R;
 import com.example.gug.base.BaseFragment;
 import com.example.gug.interfaces.IBasePresenter;
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
 
 public class ClassFragment extends BaseFragment {
+    @BindView(R.id.tl_class)
+    TabLayout tlClass;
+    @BindView(R.id.vp_class)
+    ViewPager vpClass;
+
     @Override
     protected int getLayout() {
-        return 0;
+        return R.layout.cch_fragment_class1;
     }
 
     @Override
