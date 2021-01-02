9e9436a2aea98f420f59ea4ccafeb3491c24c541
psy1273596976 <psy1273596976@163.com>
2021-01-02 19:14:04 +0800

    我的fragment提交

--- app/src/main/java/com/example/gug/MainActivity.java
+++ app/src/main/java/com/example/gug/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.gug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.gug.psy.ui.MeAcyivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, MeAcyivity.class));
+
     }
 }
---
+++ app/src/main/java/com/example/gug/psy/ui/MeFragment.java
@@ -0,0 +1,28 @@
+package com.example.gug.psy.ui;
+
+
+import com.example.gug.R;
+import com.example.gug.base.BaseFragment;
+import com.example.gug.interfaces.IBasePresenter;
+
+public class MeFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.layout_fargment_me;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
