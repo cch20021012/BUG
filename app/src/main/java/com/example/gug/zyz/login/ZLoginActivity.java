c52f360c44bed132fc576a93bef637ca1c0778c2
Apple <zyz2436867503@163.com>
2021-01-03 07:47:12 +0800

    person

---
+++ app/src/main/java/com/example/gug/zyz/login/ZLoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.gug.zyz.login;
+
+
+import com.example.gug.R;
+import com.example.gug.base.BaseActivity;
+import com.example.gug.interfaces.IBasePresenter;
+
+public class ZLoginActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+}
