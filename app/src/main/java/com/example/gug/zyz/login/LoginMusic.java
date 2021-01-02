05b7e373b178ac74e263b64778d52692b18f6b15
Apple <zyz2436867503@163.com>
2021-01-03 07:29:26 +0800

    person

---
+++ app/src/main/java/com/example/gug/zyz/login/LoginMusic.java
@@ -0,0 +1,27 @@
+package com.example.gug.zyz.login;
+
+import com.example.gug.R;
+import com.example.gug.base.BaseActivity;
+import com.example.gug.interfaces.IBasePresenter;
+
+public class LoginMusic extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_loginmusic;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
