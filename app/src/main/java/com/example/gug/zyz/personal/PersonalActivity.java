ea6e5f6034ae00a2d8db431cf47d120fa995935d
Apple <zyz2436867503@163.com>
2021-01-03 08:05:41 +0800

    person

--- app/src/main/java/com/example/gug/zyz/personal/PersonalActivity.java
+++ app/src/main/java/com/example/gug/zyz/personal/PersonalActivity.java
@@ -1,10 +1,20 @@
 package com.example.gug.zyz.personal;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.gug.R;
 import com.example.gug.base.BaseActivity;
 import com.example.gug.interfaces.IBasePresenter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class PersonalActivity extends BaseActivity {
+    @BindView(R.id.tv_cancel)
+    TextView tvCancel;
+
     @Override
     protected int getLayout() {
         return R.layout.activity_personal;
@@ -17,11 +27,23 @@ public class PersonalActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        tvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
