fee67220943eaac24085e3aab276e71d4e109d04
psy1273596976 <psy1273596976@163.com>
2021-01-07 16:35:34 +0800

    psy提交

--- app/src/main/java/com/example/gug/psy/ui/WorkTalkActivity.java
+++ app/src/main/java/com/example/gug/psy/ui/WorkTalkActivity.java
@@ -1,8 +1,13 @@
 package com.example.gug.psy.ui;
 
+import android.os.Bundle;
+
 import com.example.gug.R;
 import com.example.gug.base.BaseActivity;
 import com.example.gug.interfaces.IBasePresenter;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WorkTalkActivity extends BaseActivity {
     @Override
@@ -23,5 +28,17 @@ public class WorkTalkActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.back)
+    public void onClick() {
+        finish();
     }
 }
